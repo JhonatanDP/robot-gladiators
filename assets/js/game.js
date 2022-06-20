c428a5520a31b8c3a64ffaaf4ac7c66447c478bd
Jhonatan <jonathandiazp@gmail.com>
2022-06-20 18:57:20 -0400

    added loop and array

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,9 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
 // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+//   window.alert("Welcome to Robot Gladiators!");
+
+while (enemyHealth >0) {
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -67,8 +69,11 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
-};
+}
+}
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+for(var i = 0; i < enemyNames.length; i++){
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
